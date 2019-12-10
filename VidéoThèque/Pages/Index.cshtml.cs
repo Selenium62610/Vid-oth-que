ad57dccebac0ba69edfe2123f088985669e115c3
bettinagr@hotmail.fr <bettinagr@hotmail.fr>
2019-12-10 11:30:03 +0100

    Récupération User Connexion

--- VidéoThèque/Pages/Index.cshtml.cs
+++ VidéoThèque/Pages/Index.cshtml.cs
@@ -1,38 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using VidéoThèque.Models;
+using Xceed.Wpf.Toolkit;
 
 namespace VidéoThèque.Pages
 {
     public class IndexModel : PageModel
     {
-       /* private readonly ILogger<IndexModel> _logger;
+        // Déclaration d'un VidéoThèqueContext
+        private readonly VidéoThèque.Models.VidéoThèqueContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        // Constructeur
+        public IndexModel(VidéoThèque.Models.VidéoThèqueContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        //Permet la recherche par nom et par Genre
+        public IList<User> User { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        //Contient le texte de l'utilisateur entrée dans la zone de recherche
+        [Required(ErrorMessage = "Un identifiant est requis.")]
+        public string SearchIdentifiant { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        [Required(ErrorMessage = "Un mot de passe est requis.")]
+        //Contient le texte de l'utilisateur entrée dans la zone de recherche
+        public string SearchMdp { get; set; }
+
+        
+        
+
+
+
+        //Parmis tout les films on recherche celui qui contiens le même titre que celui dans la barre de recherche
+        public async Task OnGetAsync()
         {
+            //Récupère tout les gens de la liste  
+            var users = from u in _context.User select u;
 
-        }*/
-
-       /* private User user;
-
-        public User GetUser()
-        {
-            return user;
+                //Si la requête n'est pas nul (SearchString) la requête sur les film est modifié on affiche telle type de film
+            if (!string.IsNullOrEmpty(SearchIdentifiant) && !string.IsNullOrEmpty(SearchMdp)){
+                users = users.Where(s => SearchIdentifiant==s.Identifiant);
+                users = users.Where(s => SearchMdp == s.MotDePasse);
+            }
+            //On affiche la requête si elle est modifié, tout les films sinon.
+            User = await users.ToListAsync();
         }
 
-        public void SetUser(User value)
-        {
-            user = value;
-        }*/
     }
 }
